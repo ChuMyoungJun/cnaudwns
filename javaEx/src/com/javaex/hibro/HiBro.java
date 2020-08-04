a7a1748dcd250d683690c2a1573640975b6b7f43
sdkon <sdkon@14.32.18.5>
2020-08-04 11:09:11 +0900

    첫번째 커밋

---
+++ javaEx/src/com/javaex/hibro/HiBro.java
@@ -0,0 +1,14 @@
+package com.javaex.hibro;
+/*
+ * 목표 : Hi Bro 문자열을 출력한다
+ * 라라라라라라ㅏ
+ */
+public class HiBro {
+	
+       public static void main(String[] atgs) {
+    	   // Hi Bro 문자열 출력
+    	   System.out.println("come on~~~~");
+    	   System.out.println("Hi Bro!");
+    	   System.out.println("Hi Bro!");
+       }
+}
